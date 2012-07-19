1c04b98767374d2c363b4fb7b5f610d0c95155a0
Steven Huwig <huwigs@huwigs-desktop.(none)>
2012-07-19 16:58:20 -0400

    initial checkin of wrapper functions, very limited functionality

--- src/clojure_xslt/core.clj
+++ src/clojure_xslt/core.clj
@@ -3,116 +3,50 @@
            [javax.xml.transform.stream StreamSource StreamResult])
   (:use [clojure.java.io :only (input-stream output-stream)]))
 
-(def ^:dynamic *xsl-params* {})
-(def ^:dynamic *xsl-output-properties* {})
-(def ^:dynamic *uri-resolver* nil)
-(def ^:dynamic *transformer-factory-features* {})
-(def ^:dynamic *transformer-factory-attributes* {})
-(def ^:dynamic *error-listener* nil)
-
 (derive java.io.InputStream ::stream)
 (derive java.io.OutputStream ::stream)
 (derive java.io.Reader ::stream)
 (derive java.io.Writer ::stream)
 
+(defn transformer-factory
+  [& opts]
+  (let [{class-name :class-name
+         class-loader :class-loader
+         features :features
+         attributes :attributes
+         uri-resolver :uri-resolver
+         error-listener :error-listener}
+        (when opts (apply hash-map opts))
+        factory (if class-name
+                  (TransformerFactory/newInstance class-name class-loader)
+                  (TransformerFactory/newInstance))]
+    (when uri-resolver
+      (.setURIResolver factory uri-resolver))
+    (when error-listener
+      (.setErrorListener factory uri-resolver))
+    (doseq [[name val] features]
+      (.setFeature factory name val))
+    (doseq [[name val] attributes]
+      (.setAttribute factory name val))
+    factory))
 
-(defn make-transformer-factory
-  ([]
-     (make-transformer-factory (TransformerFactory/newInstance)))
-  ([class-name class-loader]
-     (make-transformer-factory
-      (TransformerFactory/newInstance class-name class-loader)))
-  ([factory]
-     (let [set-features (fn [tf]
-                          (doseq [[k v] *transformer-factory-features*]
-                            (.setFeature tf k v)))
-           set-attributes (fn [tf]
-                            (doseq [[k v] *transformer-factory-attributes*]
-                              (.setAttribute tf k v)))]
-       (when *uri-resolver*
-         (.setURIResolver factory *uri-resolver*))
-       (when *error-listener*
-         (.setErrorListener factory *error-listener*))
-       (set-features factory)
-       (set-attributes factory))
-     factory))
+(defn transformer
+  ([xslt-input]
+     (transformer (transformer-factory) xslt-input))
+  ([factory xslt-input]
+     (let [source (StreamSource. xslt-input)]
+       (.newTransformer factory source))))
 
-
-(defmulti make-templates class)
-
-(defmethod make-templates ::stream
-  [xslt-input]
-  (let [factory (TransformerFactory/newInstance)]
-    (let [source (StreamSource. xslt-input)]
-      (.newTemplates factory source))))
-
-(defmethod make-templates Templates
-  [xslt-input]
-  xslt-input)
-
-(defmethod make-templates :default
-  [xslt-input]
-  (let [factory (TransformerFactory/newInstance)]
-    (with-open [input (input-stream xslt-input)]
-      (let [source (StreamSource. input)]
-        (.newTemplates factory source)))))
-
-
-(defmulti make-transformer class)
-
-(defmethod make-transformer ::stream
-  [xslt-input]
-  (let [factory (TransformerFactory/newInstance)]
-    (let [source (StreamSource. xslt-input)]
-      (.newTransformer factory source))))
-
-(defmethod make-transformer Templates
-  [xslt-input]
-  (.newTransformer xslt-input))
-
-(defmethod make-transformer Transformer
-  [xslt-input]
-  xslt-input)
-
-(defmethod make-transformer :default
-  [xslt-input]
-  (let [factory (TransformerFactory/newInstance)]
-    (with-open [input (input-stream xslt-input)]
-      (let [source (StreamSource. input)]
-        (.newTransformer factory source)))))
-
+(defn templates
+  [factory xslt-input]
+  (let [source (StreamSource. xslt-input)]
+    (.newTemplates factory source)))
 
 (defn transform
-  ([xslt]
-     (transform xslt *in* *out*))
-  ([xslt in]
-     (transform xslt in *out*))
-  ([xslt in out]
-     ;; Convert arguments to streams if necessary.
-     (let [set-xsl-params (fn [transformer]
-                            (doseq [[name value] *xsl-params*]
-                              (.setParameter name value)))
-           input-is-stream? (isa? (class in) ::stream)
-           output-is-stream? (isa? (class out) ::stream)
-           input (if input-is-stream? in (input-stream in))
-           output (if output-is-stream? out (output-stream out))]
-       (try
-         (let [transformer (make-transformer xslt)
-               input-source (StreamSource. input)
-               output-result (StreamResult. output)]
-           (set-xsl-params transformer)
-           (.transform transformer input-source output-result))
-         (finally
-          ;; Close streams that this function created.
-          (when (not input-is-stream?)
-            (.close input))
-          (when (not output-is-stream?)
-            (.close output)))
-         ))))
-
-
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+  [xslt in out & xsl-params]
+  (let [source (StreamSource. in)
+        result (StreamResult. out)]
+    (doseq [[name val] xsl-params]
+      (.setParameter xslt name val))
+    (.transform xslt source result)))
 
--- test/clojure_xslt/core_test.clj
+++ test/clojure_xslt/core_test.clj
@@ -1,7 +1,27 @@
 (ns clojure-xslt.core-test
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream])
   (:use clojure.test
-        clojure-xslt.core))
+        clojure-xslt.core
+        clojure.java.io
+        clojure.xml))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest identity-transform
+  (testing "Run the XSLT identity transform against itself."
+    (let [xslt-identity-bytes (ByteArrayOutputStream.)]
+      (with-open [xslt-identity-stream (input-stream "resources/identity.xsl")]
+        (copy xslt-identity-stream xslt-identity-bytes))
+      (let [data (.toByteArray xslt-identity-bytes)
+            xslt (transformer (ByteArrayInputStream. data))
+            document-input (ByteArrayInputStream. data)
+            document-output (ByteArrayOutputStream.)]
+        (transform xslt document-input document-output)
+        (let [expected (clojure.xml/parse (ByteArrayInputStream. data))
+              actual (clojure.xml/parse (ByteArrayInputStream.
+                                         (.toByteArray document-output)))]
+          (is (= actual expected)))))))
+
+(deftest change-title-transform
+  (testing "Run sample XSLT against sample XML."
+    (with-open [xslt (input-stream "resources/change-title.xsl")
+                xml (input-stream "resources/dublin-core.xml")]
+      (transform (transformer xslt) xml *out*))))
