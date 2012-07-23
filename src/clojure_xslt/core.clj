9d08ffbb352692639836614a7ba6fc3fca730004
Steven Huwig <huwigs@huwigs-desktop.(none)>
2012-07-23 10:39:50 -0400

    add xpath convenience functions

--- src/clojure_xslt/core.clj
+++ src/clojure_xslt/core.clj
@@ -1,12 +1,56 @@
 (ns clojure-xslt.core
   (:import [javax.xml.transform TransformerFactory Transformer Templates]
-           [javax.xml.transform.stream StreamSource StreamResult])
+           [javax.xml.transform.stream StreamSource StreamResult]
+           [javax.xml.xpath XPathFactory XPathConstants])
   (:use [clojure.java.io :only (input-stream output-stream)]))
 
 (derive java.io.InputStream ::stream)
 (derive java.io.OutputStream ::stream)
 (derive java.io.Reader ::stream)
 (derive java.io.Writer ::stream)
+
+
+(def xpath-constant-map
+  {:string XPathConstants/STRING
+   :boolean XPathConstants/BOOLEAN
+   :dom-object-model XPathConstants/DOM_OBJECT_MODEL
+   :node XPathConstants/NODE
+   :nodeset XPathConstants/NODESET
+   :number XPathConstants/NUMBER})
+
+(defn xpath-factory
+  [& opts]
+  (let [{features :features
+         function-resolver :function-resolver
+         variable-resolver :variable-resolver
+         class-name :class-name
+         class-loader :class-loader
+         object-model-uri :object-model-uri}
+        (when opts (apply hash-map opts))
+        uri (or object-model-uri XPathFactory/DEFAULT_OBJECT_MODEL_URI)
+        factory (if class-name
+                  (XPathFactory/newInstance uri class-name class-loader)
+                  (XPathFactory/newInstance uri))]
+    (doseq [[name val] features]
+      (.setFeature factory name val))
+    (when function-resolver
+      (.setXPathFunctionResolver factory function-resolver))
+    (when variable-resolver
+      (.setXPathVariableResolver factory variable-resolver))
+    factory))
+
+(defn xpath
+  ([expression]
+     (xpath (xpath-factory) expression))
+  ([xpath-factory expression]
+     (xpath xpath-factory expression :string))
+  ([xpath-factory expression return-type]
+     (let [xpath-return-type
+           (xpath-constant-map return-type)]
+       (fn [obj]
+         (let [xp (.newXPath xpath-factory)]
+           (.evaluate expression obj xpath-return-type))))))
+
 
 (defn transformer-factory
   [& opts]
